01b5c3955577faff9ecb5c1fcd457fe8f4135b4b
MakuAureo <62676908+MakuAureo@users.noreply.github.com>
2020-04-19 21:57:35 -0300

    Update Chuva.js

--- Chuva.js
+++ Chuva.js
@@ -1,13 +1,12 @@
-			Chuva (com vento personalizavel)
-
+//Chuva (com vento personalizavel)
 function setup() {
   createCanvas(500, 500);
-  air = random(-10, 10);
+  wind = random(-10, 10);
   obs = [];
   for (i = 0; i < 800; i++) {
     var array = [
       [0, 1],
-      [air, random(1, 40)],
+      [wind, random(1, 40)],
       [random(-100, 600), random(-200, -50)]
     ];
     append(obs, array);
@@ -18,15 +17,15 @@ function draw() {
   background(25);
   strokeWeight(1);
   stroke(0, 110, 220);
-  air *= random(0.85, 1.15);
-  if (air > 10 || air < -10) {
-    air *= random(0.7, 0.9);
+  wind *= random(0.85, 1.15);
+  if (wind > 10 || wind < -10) {
+    wind *= random(0.7, 0.9);
   }
-  if (air > -0.8 & air < 0.8) {
-    air *= -random(1.1, 1.15);
+  if (wind > -0.8 & wind < 0.8) {
+    wind *= -random(1.1, 1.15);
   }
   for (a = 0; a < obs.length; a++) {
-    line(obs[a][2][0], obs[a][2][1], obs[a][2][0] + air, obs[a][2][1] + 15);
+    line(obs[a][2][0], obs[a][2][1], obs[a][2][0] + wind, obs[a][2][1] + 15);
   }
   for (i = 0; i < obs.length; i++) {
     for (j = 1; j < obs[0].length; j++) {
@@ -39,7 +38,7 @@ function draw() {
     if (obs[l][2][1] > 500) {
       obs[l] = [
         [0, 1],
-        [air, random(1, 40)],
+        [wind, random(1, 40)],
         [random(-100, 600), -15]
       ];
     }
